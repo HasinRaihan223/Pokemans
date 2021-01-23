0083d4705d8d664413f4b0c5eefcce0cd0cfb6ab
HasinRaihan223 <hraihan20@stuy.edu>
2021-01-22 22:00:54 -0500

    Started setUp

--- Game.java
+++ Game.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class Game {
-  
+
+  Scanner myScanner = new Scanner(System.in);
+  String playerName;
+
+  public static void main(String[]args){
+    Game newGame = new Game();
+    newGame.setUp();
+  }
+
+  public void setUp(){
+    System.out.println();
+    System.out.println("------------------------");
+    System.out.println("Welcome to Pokemon Game!");
+    System.out.println();
+    System.out.println("Please enter your name:");
+		playerName = myScanner.nextLine();
+    System.out.println();
+    System.out.println("Welcome " + playerName + " to my pokemon game!");
+  }
+
 }
