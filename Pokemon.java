f874507024b8f7674c76692d8a43b1bb5b8a9ae0
Andy <alin21@stuy.edu>
2021-01-26 10:22:57 -0500

    changed name back to pokemon and fixed issue

--- Pokemon.java
+++ Pokemon.java
@@ -1,24 +1,21 @@
 public class Pokemon {
-  private int level, exp, hp, baseDef, baseAttack, currentHp, currentBaseDef, currentBaseAttack, levelToEvolve;
-  private String[] attacks, sprites, type, abilities;
-  private String heldItems, name, nickname;
-  private Pokemon[] evolutions;
+  private int level, exp, hp, baseDef, baseAttack, currentHp, currentBaseDef, currentBaseAttack;
+  private String[] attacks, type, abilities;
+  private String name;
+  // private Pokemon[] evolutions;
 
-  public Pokemon(int level, int exp, int hp, int baseDef, int baseAttack, int levelToEvolve, String[] attacks,
-  String[] sprites, String[] types, String name, Pokemon[] evolutions){
+  public Pokemon(int level, int exp, int hp, int baseDef, int baseAttack, String[] attacks, String[] type, String name){
     this.level = level;
     this.exp = exp;
     this.hp = hp;
-    this.currentHp = hp;
+    // this.currentHp = hp;
     this.baseDef = baseDef;
-    this.currentBaseDef = baseDef;
-    this.currentBaseAttack = baseAttack;
+    // this.currentBaseDef = baseDef;
+    // this.currentBaseAttack = baseAttack;
     this.baseAttack = baseAttack;
     this.name = name;
-    this.nickname = name;
-    this.sprites = sprites;
+    // this.nickname = name;
     this.attacks = attacks;
-    this.evolutions = evolutions;
   }
 
   public int maxHealth(){
@@ -53,17 +50,17 @@ public class Pokemon {
     return this.currentBaseAttack;
   }
 
-  public int levelToEvolve(){
-    return this.levelToEvolve();
-  }
+  // public int levelToEvolve(){
+  //   return this.levelToEvolve();
+  // }
 
   public String name(){
     return this.name;
   }
 
-  public String nickname(){
-    return this.nickname;
-  }
+  // public String nickname(){
+  //   return this.nickname;
+  // }
 
   public int changeHealth(int change){
     this.currentHp += change;
